e80c91989a692704373fd098e43994bfcea7849a
Alina Ghani <alina@MacBook-Air-de-Alina.local>
2024-01-16 22:38:13 +0100

    push

--- script_launch_app.sh
+++ script_launch_app.sh
@@ -2,8 +2,12 @@
 
 cd /Users/alina/vs-project/project/pipeline_ml
 
+export FLASK_APP=app.py
+
 git pull origin main
+
 
 pip install -r requirements.txt
 
+# Run the Flask app
 flask run --host='0.0.0.0' --port=8081
