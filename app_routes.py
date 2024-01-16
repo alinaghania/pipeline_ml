0e961a5fc5d97ffadfb506ce604161fe876f11a8
Alina Ghani <alina@MacBook-Air-de-Alina.local>
2024-01-16 19:40:24 +0100

    push

--- app_routes.py
+++ app_routes.py
@@ -1,42 +1,27 @@
 from flask import Blueprint, jsonify, request
-import os
 import subprocess
 
 deployment_blueprint = Blueprint('deployment', __name__)
 testing_blueprint = Blueprint('testing', __name__)
 
+# Path to your scripts
+PATH_TO_TEST_SCRIPT = '/Users/alina/vs-project/project/pipeline_ml/script_launch_app.sh'
+PATH_TO_LAUNCH_SCRIPT = '/Users/alina/vs-project/project/pipeline_ml/script_run_test.sh'
+
 @testing_blueprint.route("/", methods=['POST'])
 def testing():
-    # Extract the JSON payload
-    payload = request.get_json()
-    print(f"Received payload: {payload}")  # Log the received payload
-
-    # Extract the branch name from the payload
-    branch_name = payload.get('ref', '')
-    print(f"Extracted branch name: {branch_name}")  # Log the extracted branch name
-
-    if branch_name == 'refs/heads/staging':
-        os.system('git pull origin staging')
-        return jsonify({'success': True}), 200, {'ContentType': 'application/json'}
-    
-    return jsonify({'success': False}), 400, {'ContentType': 'application/json'} 
+    try:
+        # Run the test script
+        subprocess.run([PATH_TO_TEST_SCRIPT], check=True)
+        return jsonify({'success': True, 'message': 'Tests started.'}), 200
+    except subprocess.CalledProcessError as e:
+        return jsonify({'success': False, 'message': str(e)}), 500
 
 @deployment_blueprint.route("/", methods=['POST'])
 def deployment():
-    # Extract the JSON payload
-    payload = request.get_json()
-    print(f"Received payload: {payload}")  # Log the received payload
-
-    # Extract the branch name from the payload
-    branch_name = payload.get('ref', '')
-    print(f"Extracted branch name: {branch_name}")  # Log the extracted branch name
-
-    if branch_name == 'refs/heads/main':
-        try:
-            subprocess.check_output(['git', 'pull', 'origin', 'main'])
-            return jsonify({'success': True}), 200
-        except subprocess.CalledProcessError as e:
-            print(f"Error pulling from main: {e.output}")
-            return jsonify({'success': False, 'error': str(e)}), 500
-    
-    return jsonify({'success': False}), 400
+    try:
+        # Run the deployment script
+        subprocess.run([PATH_TO_LAUNCH_SCRIPT], check=True)
+        return jsonify({'success': True, 'message': 'Deployment started.'}), 200
+    except subprocess.CalledProcessError as e:
+        return jsonify({'success': False, 'message': str(e)}), 500
