bf579c1db57a98a9b86d6c775092db627fbb7b2b
Alina Ghani <alina@MacBook-Air-de-Alina.local>
2024-01-17 00:38:55 +0100

    push

--- app_routes.py
+++ app_routes.py
@@ -4,39 +4,36 @@ import subprocess
 deployment_blueprint = Blueprint('deployment', __name__)
 testing_blueprint = Blueprint('testing', __name__)
 
-# Paths to your scripts
+# Chemins vers vos scripts
 PATH_TO_TEST_SCRIPT = '/Users/alina/vs-project/project/pipeline_ml/script_run_test.sh'
 PATH_TO_LAUNCH_SCRIPT = '/Users/alina/vs-project/project/pipeline_ml/script_launch_app.sh'
 
-@deployment_blueprint.route("/deployment", methods=['POST'])
+@deployment_blueprint.route("", methods=['POST'])
 def deployment():
     try:
-        # Extract the branch name from the payload
+        # Extraire le nom de la branche à partir des données de la requête
         data = request.json
-        branch_name = data['ref'].split('/')[-1]
+        branch_name = data['ref'].split('/')[-1]  # 'refs/heads/nom_de_la_branche'
 
-        # Check if the push was to the main branch
+        # Exécuter le script avec le nom de la branche pour la branche 'main'
         if branch_name == "main":
-            # Run the deployment script
-            subprocess.run([PATH_TO_LAUNCH_SCRIPT,branch_name], check=True)
+            subprocess.run([PATH_TO_LAUNCH_SCRIPT, branch_name], check=True)
             return jsonify({'success': True, 'message': 'Deployment started.'}), 200
-        else:
-            return jsonify({'success': False, 'message': 'Deployment skipped for non-main branch.'}), 200
+        return jsonify({'success': False, 'message': 'Deployment not required for this branch.'}), 200
     except subprocess.CalledProcessError as e:
         return jsonify({'success': False, 'message': str(e)}), 500
-@testing_blueprint.route("/testing", methods=['POST'])
+
+@testing_blueprint.route("", methods=['POST'])
 def testing():
     try:
-        # Extract the branch name from the payload
+        # Extraire le nom de la branche à partir des données de la requête
         data = request.json
         branch_name = data['ref'].split('/')[-1]
 
-        # Check if the push was to the staging branch
+        # Exécuter le script avec le nom de la branche pour la branche 'staging'
         if branch_name == "staging":
-            # Run the testing script
-            subprocess.run([PATH_TO_TEST_SCRIPT,branch_name], check=True)
+            subprocess.run([PATH_TO_TEST_SCRIPT, branch_name], check=True)
             return jsonify({'success': True, 'message': 'Testing started.'}), 200
-        else:
-            return jsonify({'success': False, 'message': 'Testing skipped for non-staging branch.'}), 200
+        return jsonify({'success': False, 'message': 'Testing not required for this branch.'}), 200
     except subprocess.CalledProcessError as e:
         return jsonify({'success': False, 'message': str(e)}), 500
