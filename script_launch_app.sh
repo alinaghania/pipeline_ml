952aab02a87420f9c21be64ae7178b28701f0bfd
Alina Ghani <alina@MacBook-Air-de-Alina.local>
2024-01-16 22:49:08 +0100

    push

--- script_launch_app.sh
+++ script_launch_app.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
-cd /Users/alina/vs-project/project/pipeline_ml
+# Enable verbose mode
+set -x
 
+# Redirect stderr to a log file
+exec 2>error_log.txt
+
+# Change to the directory of the script
+cd "$(dirname "$0")"
+
+# Set the FLASK_APP environment variable
 export FLASK_APP=app.py
 
+# Pull the latest changes from the main branch
 git pull origin main
 
-
+# Install required Python packages
 pip install -r requirements.txt
 
 # Run the Flask app
-nohup flask run --host='0.0.0.0' --port=8081
+flask run --host='0.0.0.0' --port=8081
